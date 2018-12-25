c4e15f29caaa7ee731459380c330de187b0f9954
Tianchi Liu <ltcwendell@gmail.com>
2018-12-25 14:54:10 +0800

    add ssl support

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<p>Hello world!</p>")
-	})
-	http.ListenAndServe(":80", nil)
+	log.Println("Go server started.")
+	http.HandleFunc("/", index)
+	go func() {
+		log.Fatalln(http.ListenAndServe(":80", nil))
+	}()
+
+	log.Fatalln(http.ListenAndServeTLS(":443", "./cert.pem", "./key.pem", nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<p>Hello world!</p>")
 }
