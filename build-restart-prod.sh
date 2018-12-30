6ff5ea0d3e81e55bcd5b3513c09d30a47781afc0
Tianchi Liu <ltcwendell@gmail.com>
2018-12-31 00:11:15 +0800

    c

--- build-restart-prod.sh
+++ build-restart-prod.sh
@@ -1,1 +1,1 @@
-go build -o main.out && systemctl restart helloworld-api.service && systemctl status helloworld-api.service
+systemctl restart helloworld-api.service && systemctl status helloworld-api.service
